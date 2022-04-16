bd1340d5cfee3d9b83efa27dca30ff31a070881b
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2022-04-16 20:26:49 +0300

    SoapServis

---
+++ AliCanYucel_SOAP/AliCanYucel_SOAP/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace AliCanYucel_SOAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnXmlVeriGonder = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtHata = new System.Windows.Forms.TextBox();
+            this.txtGelenCevap = new System.Windows.Forms.TextBox();
+            this.btnBaglantiTest = new System.Windows.Forms.Button();
+            this.btnJsonVeriGonder = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnXmlVeriGonder
+            // 
+            this.btnXmlVeriGonder.Location = new System.Drawing.Point(127, 415);
+            this.btnXmlVeriGonder.Name = "btnXmlVeriGonder";
+            this.btnXmlVeriGonder.Size = new System.Drawing.Size(132, 23);
+            this.btnXmlVeriGonder.TabIndex = 0;
+            this.btnXmlVeriGonder.Text = "XML VERİ GÖNDER";
+            this.btnXmlVeriGonder.UseVisualStyleBackColor = true;
+            this.btnXmlVeriGonder.Click += new System.EventHandler(this.btnXmlVeriGonder_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txtGelenCevap);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 396);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Response";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtHata);
+            this.groupBox2.Location = new System.Drawing.Point(7, 199);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(762, 171);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Program İşelyişinde Karşılaşılan Hatalar";
+            // 
+            // txtHata
+            // 
+            this.txtHata.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtHata.Location = new System.Drawing.Point(6, 19);
+            this.txtHata.Multiline = true;
+            this.txtHata.Name = "txtHata";
+            this.txtHata.ReadOnly = true;
+            this.txtHata.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtHata.Size = new System.Drawing.Size(750, 136);
+            this.txtHata.TabIndex = 4;
+            this.txtHata.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtGelenCevap
+            // 
+            this.txtGelenCevap.Location = new System.Drawing.Point(6, 19);
+            this.txtGelenCevap.Multiline = true;
+            this.txtGelenCevap.Name = "txtGelenCevap";
+            this.txtGelenCevap.ReadOnly = true;
+            this.txtGelenCevap.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtGelenCevap.Size = new System.Drawing.Size(763, 173);
+            this.txtGelenCevap.TabIndex = 2;
+            // 
+            // btnBaglantiTest
+            // 
+            this.btnBaglantiTest.Location = new System.Drawing.Point(514, 415);
+            this.btnBaglantiTest.Name = "btnBaglantiTest";
+            this.btnBaglantiTest.Size = new System.Drawing.Size(132, 23);
+            this.btnBaglantiTest.TabIndex = 3;
+            this.btnBaglantiTest.Text = "BAĞLANTI TEST";
+            this.btnBaglantiTest.UseVisualStyleBackColor = true;
+            this.btnBaglantiTest.Click += new System.EventHandler(this.btnBaglantiTest_Click);
+            // 
+            // btnJsonVeriGonder
+            // 
+            this.btnJsonVeriGonder.Location = new System.Drawing.Point(320, 415);
+            this.btnJsonVeriGonder.Name = "btnJsonVeriGonder";
+            this.btnJsonVeriGonder.Size = new System.Drawing.Size(132, 23);
+            this.btnJsonVeriGonder.TabIndex = 4;
+            this.btnJsonVeriGonder.Text = "JSON VERİ GÖNDER";
+            this.btnJsonVeriGonder.UseVisualStyleBackColor = true;
+            this.btnJsonVeriGonder.Click += new System.EventHandler(this.btnJsonVeriGonder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnJsonVeriGonder);
+            this.Controls.Add(this.btnBaglantiTest);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnXmlVeriGonder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnXmlVeriGonder;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtGelenCevap;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtHata;
+        private System.Windows.Forms.Button btnBaglantiTest;
+        private System.Windows.Forms.Button btnJsonVeriGonder;
+    }
+}
+
---
+++ AliCanYucel_SOAP/AliCanYucel_SOAP/Form1.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Xml;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace AliCanYucel_SOAP
+{
+    public partial class Form1 : Form
+    {
+        const string servisUrl = "http://10.20.30.125:8000/sap/bc/srt/rfc/sap/zsd_ws_create_sales_order/100/zsd_ws_create_sales_order/zsd_ws_create_sales_order";
+        const string kullanilacakActionUrl = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WebServiseBaglanVeXmlGonder()
+        {
+            XmlDocument xmlDokuman = XmlDokumanOlustur();
+            HttpWebRequest webRequest = RequestOlustur(servisUrl, kullanilacakActionUrl);
+            WebRequestIcerikEkle(xmlDokuman, webRequest);
+
+            try
+            {
+                IAsyncResult cevap = webRequest.BeginGetResponse(null, null);
+                cevap.AsyncWaitHandle.WaitOne();
+
+                string xmlCevap;
+                using (WebResponse webResponse = webRequest.EndGetResponse(cevap))
+                {
+                    using (StreamReader streamReader = new StreamReader(webResponse.GetResponseStream()))
+                    {
+                        xmlCevap = streamReader.ReadToEnd();
+                    }
+                    txtGelenCevap.Text = xmlCevap;
+                }
+            }
+            catch (Exception hata)
+            {
+                txtHata.AppendText("Metod [WebServiseBaglanVeGonder] Hata : \r\n" + hata.Message);
+                txtHata.AppendText(Environment.NewLine);
+                txtHata.AppendText(Environment.NewLine);
+            }
+
+
+        }
+
+        private HttpWebRequest RequestOlustur(string _servisUrl, string _kullanilacakActionUrl)
+        {
+            
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(_servisUrl);
+
+            try
+            {
+                webRequest.Headers.Add("SOAPAction", _kullanilacakActionUrl);
+                webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+                webRequest.Accept = "text/xml";
+                webRequest.Method = "POST";
+            }
+            catch (Exception hata)
+            {
+                txtHata.AppendText("Metod [RequestOlustur] Hata : \r\n" + hata.Message);
+                txtHata.AppendText(Environment.NewLine);
+                txtHata.AppendText(Environment.NewLine);
+            }
+            
+            
+            return webRequest;
+        }
+
+        private void WebRequestIcerikEkle(XmlDocument xmlDokuman, HttpWebRequest webRequest)
+        {
+            try
+            {
+                using (Stream stream = webRequest.GetRequestStream())
+                {
+                    xmlDokuman.Save(stream);
+                }
+            }
+            catch (Exception hata)
+            {
+                txtHata.AppendText("Metod [WebRequestIcerikEkle] Hata : \r\n"+hata.Message);
+                txtHata.AppendText(Environment.NewLine);
+                txtHata.AppendText(Environment.NewLine);
+            }
+
+        }
+
+       
+        private XmlDocument XmlDokumanOlustur()
+        {
+            XmlDocument xmlDokuman = new XmlDocument();
+            var xmlBilgi = @"<?xml version=""1.0"" encoding=""UTF - 8""?>
+ <soapenv:Envelope xmlns:soapenv = 'http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn = 'urn:sap-com:document:sap:soap:functions:mc-style'>
+        <soapenv:Header></soapenv:Header>
+         <soapenv:Body>       
+             <urn:Zsd002FmCreateSalesOrder>         
+                 <!--Optional:-->          
+                  <ItAddress>          
+                     <!--Zero or more repetitions:-->            
+                       <item>            
+                         <Source>TEKNOLIST</Source>            
+                          <Guid>1000000079</Guid>            
+                          <GuidVer />            
+                          <Parvw>WE</Parvw>            
+                          <AddrNo>1</AddrNo>            
+                          <Formofaddr />            
+                          <Name />
+                          <Name2 />
+                          <Name3 />
+                          <Name4 />
+                          <COName />
+                          <City>İSTANBUL</City>
+                          <District>ATAŞEHİR</District>
+                          <CityNo />
+                          <PostlCod1 />
+                          <PostlCod2 />
+                          <PostlCod3 />
+                          <PoBox />
+                          <PoBoxCit />
+                          <DelivDis />
+                          <Street>İÇERENKÖY MAHALLESI 2893 SOKAK NO 1</Street>
+                          <StreetNo />
+                          <StrAbbr />
+                          <HouseNo />
+                          <StrSuppl1 />
+                          <StrSuppl2 />
+                          <Location />
+                          <Building />
+                          <Floor />
+                          <RoomNo />
+                          <Country>TR</Country>
+                          <Langu>T</Langu>
+                          <Region>34</Region>
+                          <Sort1 />
+                          <Sort2 />
+                          <TimeZone />
+                          <Taxjurcode />
+                          <AdrNotes />
+                          <CommType />
+                          <Tel1Numbr />
+                          <Tel1Ext />
+                          <FaxNumber />
+                          <FaxExtens />
+                          <StreetLng />
+                          <DistrctNo />             
+                          <Chckstatus />
+                          <PboxcitNo />
+                          <Transpzone />
+                          <HouseNo2 />
+                          <EMail />
+                          <StrSuppl3 />
+                          <Title />
+                          <Countryiso />
+                          <LanguIso />
+                          <BuildLong />
+                          <Regiogroup />
+                          <HomeCity />
+                          <Homecityno />
+                          <Pcode1Ext />
+                          <Pcode2Ext />
+                          <Pcode3Ext />
+                          <PoWONo />
+                          <PoBoxReg />
+                          <PoboxCtry />
+                          <PoCtryiso />
+                          <Homepage />
+                          <DontUseS />
+                          <DontUseP />
+                          <HouseNo3 />
+                          <LanguCr />
+                          <Langucriso />
+                          <PoBoxLobby />
+                          <DeliServType />
+                          <DeliServNumber />
+                          <UriType />
+                          <CountyCode />
+                          <County />
+                          <TownshipCode />
+                          <Township />
+                          <Xpcpt />
+                       </item>
+                    </ItAddress>
+               
+                       <!--Optional:-->
+                
+                        <ItHeader>
+                
+                           <!--Zero or more repetitions:-->
+                  
+                             <item>
+                  
+                              <Source>TEKNOLIST</Source>
+                  
+                                <Guid>1000000079</Guid>
+                  
+                                <GuidVer />
+                  
+                                <PoId>CUSTOMER REF02</PoId>
+                     
+                                   <Auart>ZM01</Auart>
+                     
+                                   <Vkorg>2920</Vkorg>
+                     
+                                   <Vtweg>10</Vtweg>
+                     
+                                   <Spart>10</Spart>
+                     
+                                   <Vkbur>2920</Vkbur>
+                     
+                                   <Vgbel />
+                     
+                                   <Vgtyp />
+                     
+                                   <Waerk>TRY</Waerk>
+                     
+                                   <Audat>20220201</Audat>
+                     
+                                   <Vdatu>20220202</Vdatu>
+                     
+                                   <CargoNo>1234567890</CargoNo>
+                     
+                                   <ShpComp>MNG KARGO</ShpComp>
+                        
+                                      <Annotation />
+                        
+                                      <Vbeln />
+                        
+                                      <ErrorText />
+                        
+                                   </item>
+                        
+                                </ItHeader>
+                        
+                                <!--Optional:-->
+                         
+                                 <ItKalem>
+                         
+                                    <!--Zero or more repetitions:-->
+                           
+                                      <item>
+                           
+                                         <Source>TEKNOLIST</Source>
+                           
+                                         <Guid>1000000079</Guid>
+                           
+                                         <GuidVer />
+                           
+                                         <Posnr>000010</Posnr>
+                           
+                                         <Vgbel />
+                           
+                                         <Vgpos />
+                           
+                                         <Vgtyp />
+                           
+                                         <Matnr>3000000015</Matnr>
+                           
+                                         <Kdmat>MTKSA4FCS060</Kdmat>
+                           
+                                         <KdmatShortText>Deneme Ürün</KdmatShortText>
+                              
+                                            <Kwmeng>100</Kwmeng>
+                              
+                                            <Vrkme>ST</Vrkme>
+                              
+                                            <Kschl>TRY</Kschl>
+                              
+                                            <Kbetr />
+                              
+                                            <Waers>TRY</Waers>
+                              
+                                            <Vbeln />
+                              
+                                            <Text />
+                              
+                                            <PageNum>10</PageNum>
+                              
+                                         </item>
+                              
+                                      </ItKalem>
+                              
+                                      <!--Optional:-->
+                               
+                                       <ItMuhatap>
+                               
+                                          <!--Zero or more repetitions:-->
+                                 
+                                            <item>
+                                 
+                                              <Source>TEKNOLIST</Source>
+                                 
+                                               <Guid>1000000079</Guid>
+                                 
+                                               <GuidVer />
+                                 
+                                               <Parvw>SV</Parvw>
+                                 
+                                               <Kunnr>1000000004</Kunnr>
+                                 
+                                               <Vbeln />
+                                 
+                                               <PoId />
+                                 
+                                            </item>
+                                 
+                                             <item>
+                                 
+                                              <Source>TEKNOLIST</Source>
+                                 
+                                               <Guid>1000000079</Guid>
+                                 
+                                               <GuidVer />
+                                 
+                                               <Parvw>MG</Parvw>
+                                 
+                                               <Kunnr>1000000004</Kunnr>
+                                 
+                                               <Vbeln />
+                                 
+                                               <PoId />
+                                 
+                                            </item>
+                                 
+                                         </ItMuhatap>
+                                 
+                                         <!--Optional:-->
+                                  
+                                          <IvSimulation>X</IvSimulation>
+                                  
+                                       </urn:Zsd002FmCreateSalesOrder>
+                                   
+                                     </soapenv:Body>
+                                   </soapenv:Envelope>
+";
+            
+            try
+            {
+                xmlDokuman.LoadXml(xmlBilgi);
+            }
+            catch (Exception hata)
+            {
+                txtHata.AppendText("Metod [XmlDokumanOlustur] Hata : \r\n" + hata.Message);
+                txtHata.AppendText(Environment.NewLine);
+                txtHata.AppendText(Environment.NewLine);
+            }
+            
+            return xmlDokuman;
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnBaglantiTest_Click(object sender, EventArgs e)
+        {
+            txtGelenCevap.Clear();
+            var client = new HttpClient();
+            txtGelenCevap.Text = "Bağlantı Kuruluyor Lütfen Bekleyiniz...";
+            try
+            {
+                var cevap = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, servisUrl));
+                txtGelenCevap.AppendText(cevap.ToString());
+            }
+            catch (Exception hata)
+            {
+                txtGelenCevap.Clear();
+                txtHata.AppendText("Bağlantı Test Hata : \r\n" + hata.ToString());
+                txtHata.AppendText(Environment.NewLine);
+                txtHata.AppendText(Environment.NewLine);
+            }
+            
+        }
+
+        private void btnXmlVeriGonder_Click(object sender, EventArgs e)
+        {
+            txtHata.Clear();
+            txtGelenCevap.Clear();
+            WebServiseBaglanVeXmlGonder();
+        }
+
+        private void btnJsonVeriGonder_Click(object sender, EventArgs e)
+        {
+            txtHata.Clear();
+            txtGelenCevap.Clear();
+        }
+    }
+}
